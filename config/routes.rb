ee83722d29f43beea1cbbc45250b77108046b0e9
Baha Manai <bahaeddinmanai7@gmail.com>
2024-08-27 09:47:07 +0100

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get "about", to: "about#index", as: :about
+
+  root to: "main#index"
+
+
+
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
