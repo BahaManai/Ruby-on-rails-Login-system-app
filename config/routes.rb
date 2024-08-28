8a8fbc3677587a413f35d4ad73b9ab4114a26fbc
Baha Manai <bahaeddinmanai7@gmail.com>
2024-08-28 12:39:39 +0100

    Forgot password

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,18 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
   delete "logout", to: "sessions#destroy"
+
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "password/reset", to: "password_resets#new"
+  get "password/reset", to: "password_resets#create"
+
   root to: "main#index"
 
 
