688e35f0ef0e45c6182b769d06addc700e3f8e83
Baha Manai <bahaeddinmanai7@gmail.com>
2024-08-28 12:56:56 +0100

    Saving updates

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,5 +3,10 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      redirect_to root_path, notice: "A link to reset your password was sent to the email you entered."
+    else
+    end
   end
 end
